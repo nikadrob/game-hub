6d736ed7a57297dd630e8f9e48269701251ce622
snasteski <nikadrob@live.com>
2024-08-20 13:28:15 +0200

    Create custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ function App() {
       templateAreas={{
         base: `"nav" "main"`, // mobile
         lg: `"nav nav" "aside main"`, // larger than 1024px
-        xl: '80em', // ~1280px
-        '2xl': '96em', // ~1536px
       }}
     >
       <GridItem area="nav">
